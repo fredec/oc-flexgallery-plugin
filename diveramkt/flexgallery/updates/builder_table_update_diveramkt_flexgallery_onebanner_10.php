c9f3f261faea29bd0181f88e0c97aac4b4e602aa
Frederico Marinho <fredericomarinho@hotmail.com>
2019-04-20 19:06:01 -0300

    - A second link (button) in banners

---
+++ diveramkt/flexgallery/updates/builder_table_update_diveramkt_flexgallery_onebanner_10.php
@@ -0,0 +1,27 @@
+<?php namespace Diveramkt\Flexgallery\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateDiveramktFlexgalleryOnebanner10 extends Migration
+{
+    public function up()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->string('link2', 255);
+            $table->string('btn_label2', 20);
+            $table->string('btn_color2', 10);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->dropColumn('link2');
+            $table->dropColumn('btn_label2');
+            $table->dropColumn('btn_color2');
+        });
+    }
+}
---
+++ diveramkt/flexgallery/updates/builder_table_update_diveramkt_flexgallery_onebanner_11.php
@@ -0,0 +1,27 @@
+<?php namespace Diveramkt\Flexgallery\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateDiveramktFlexgalleryOnebanner11 extends Migration
+{
+    public function up()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->string('link2', 255)->nullable()->change();
+            $table->string('btn_label2', 20)->nullable()->change();
+            $table->string('btn_color2', 10)->nullable()->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->string('link2', 255)->nullable(false)->change();
+            $table->string('btn_label2', 20)->nullable(false)->change();
+            $table->string('btn_color2', 10)->nullable(false)->change();
+        });
+    }
+}
