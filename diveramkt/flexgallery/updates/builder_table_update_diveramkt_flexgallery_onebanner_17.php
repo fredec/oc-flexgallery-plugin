e065bb05ff341ad163136c46d6f235bfd7d45037
fredec <fredericomarinho@hotmail.com>
2020-09-11 11:44:20 -0300

    field class in banners

---
+++ diveramkt/flexgallery/updates/builder_table_update_diveramkt_flexgallery_onebanner_17.php
@@ -0,0 +1,27 @@
+<?php namespace Diveramkt\Flexgallery\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateDiveramktFlexgalleryOnebanner17 extends Migration
+{
+    public function up()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->string('btn_class')->nullable();
+            $table->string('btn2_class')->nullable();
+            $table->text('links')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('diveramkt_flexgallery_onebanner', function($table)
+        {
+            $table->dropColumn('btn_class');
+            $table->dropColumn('btn2_class');
+            $table->dropColumn('links');
+        });
+    }
+}
